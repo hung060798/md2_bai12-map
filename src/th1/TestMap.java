2c2177b6c674d03120f28d64faa7678049ae578d
hung060798 <hungpppp122gmail.com>
2021-06-24 11:43:56 +0700

    map

---
+++ src/th1/TestMap.java
@@ -0,0 +1,30 @@
+package th1;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TestMap {
+    public static void main(String[] args) {
+        Map<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("hung", 18);
+        hashMap.put("hoang", 19);
+        hashMap.put("linh",15 );
+        hashMap.put("toan ", 8);
+        hashMap.put("anh", 3);
+        System.out.println("display entries in hashMap");
+        System.out.println(hashMap + "\n");
+
+        Map<String, Integer> treeMap = new TreeMap<>(hashMap);
+        System.out.println("Display entries in ascending order of key");
+        System.out.println(treeMap);
+
+        Map<String, Integer> linkedHashMap = new LinkedHashMap<>(16, 0.75f, true);
+        linkedHashMap.put("Smith", 30);
+        linkedHashMap.put("Anderson", 31);
+        linkedHashMap.put("Lewis", 29);
+        linkedHashMap.put("Cook", 29);
+        System.out.println("\nThe age for " + "Lewis is " + linkedHashMap.get("Smith"));
+    }
+}
